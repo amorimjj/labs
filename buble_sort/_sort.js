f4681c26c425f9f3c46a33425ef7b8a2fd47304f
amorimjj <amorimjj@gmail.com>
2018-03-09 17:48:33 -0300

    Refactory for use recursive function

--- buble_sort/_sort.js
+++ buble_sort/_sort.js
@@ -1,25 +1,35 @@
 'use strict';
 
+let swap = function(arr, i, j) {
+	arr[i] = arr[i] + arr[j];
+	arr[j] = arr[i] - arr[j];
+	arr[i] = arr[i] - arr[j];
+}
+
+
+let _do = function(toOrder, i, flagged, compareFunction) {
+
+	let n = i+1, current = toOrder[i], next = toOrder[n];
+
+	if ( compareFunction(current, next) ) {
+        swap(toOrder, i, n);
+		flagged = true;
+	}
+	
+	if ( i < toOrder.length ) {
+		return _do(toOrder, n, flagged, compareFunction);
+	}
+
+	if ( flagged ) {
+		return _do(toOrder, 0, false, compareFunction);
+	}
+
+	return toOrder;
+
+}
+
 let order = function(toOrder, compareFunction) {
-
-    let i = 0, flagged = true,  current, next;
-
-
-    while ( flagged ) {
-
-        flagged = false;
-
-        for ( i = 0; i < toOrder.length; i++  ) {
-            current = toOrder[i], next = toOrder[i+1];
-            if ( compareFunction(current,  next) ) {
-                toOrder[i] = next;
-                toOrder[i+1] = current;
-                flagged = true;
-            }
-        }
-    }
-
-    return toOrder;
+	return _do(toOrder, 0, false, compareFunction);
 };
 
 module.exports = order;
